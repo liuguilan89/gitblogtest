51617f5cefeba859b0bb373323dedb132790ebf9
liuguilan <1026640781@qq.com>
2015-12-13 15:47:35 +0800

    Initial commit

---
+++ src/src/main/java/test.java
@@ -0,0 +1,12 @@
+/**
+ * Created by liuguilan on 2015/12/11.
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("successful!");
+        System.out.println("123");
+    }
+
+
+}
